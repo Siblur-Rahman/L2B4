3992cc0d7a33d16b938f36054d5827fe8257f652
MS Rahman <msrahman048gmail.com>
2024-11-01 16:55:37 +0600

    first commit

---
+++ Typescript-Technocrat/module01/src/1.4basicDatatypes.ts
@@ -0,0 +1,40 @@
+// Primitive Data types Declare
+
+// string
+let firstName : string = 'Siblur Rahman'
+
+//number
+let rollNumber: number = 123
+
+//boolean
+let isAdmin : boolean = true
+
+// undefined
+let y: undefined = undefined
+
+// null
+let z: null = null
+
+let d; // It means that we can assign any datatype data in the variable, but it shouldn't be declared like this. we should declare with data Type
+
+d = 123
+
+d = 'abc'
+
+d = true
+
+// Non Primitive Data types Declare
+
+
+// Array
+let friend : string[] = ['saif', 'shamim']
+let rollList : number[] = [1123, 3455, 5678]
+
+// tuple --> array --> order --> type of value
+
+let coordinates : [number, number] = [1, 5]
+let ageName:[number, string] = [42, 'siblur rahman']
+let inTrreeTypesData:[number, string, boolean] =[42, 'siblur rahman', true]
+
+
+
---
+++ Typescript-Technocrat/module01/src/1.51-5 ObjectOptionalLiteralTypes.ts
@@ -0,0 +1,7 @@
+// Reference Type ---> Object
+
+const user = {
+    firstName:'MD.',
+    middleName:'SIBLUR',
+    lastName:'RAHMAN'
+}
